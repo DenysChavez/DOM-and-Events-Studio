bbcc94a65c50b5fefc92b6342fa2dba3b74d1a7b
Denys <go4denys@gmail.com>
2023-02-02 12:28:15 -0500

    set the takeOff button

--- scripts.js
+++ scripts.js
@@ -1,2 +1,22 @@
 // Write your JavaScript code here.
+function init() {
+    let takeOffButton = document.getElementById("takeoff");
+    let flightStatus = document.getElementById("flightStatus");
+    let shutteBackground = document.getElementById("shuttleBackground");
+    let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+    let shutte = document.getElementById("shutte");
+
+    takeOffButton.addEventListener("click", event => {
+        let responde = window.confirm("Confirm that the shutte is ready for takeoff");
+        if (responde) {
+            flightStatus.innerHTML = "Shuttle in flight.";
+            shutteBackground.style.backgroundColor = "blue";
+            shutte.setAttribute("style", "position: absolute");
+            spaceShuttleHeight.innerHTML = 10000;
+        }
+    })
+
+} 
+
+window.addEventListener("load", init);
 // Remember to pay attention to page loading!
